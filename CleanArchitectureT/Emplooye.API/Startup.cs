89e2fd4b4a110a7508d6df30901626d635a917f6
omidsn <omid.snnnn@gmail.com>
2022-08-20 09:34:36 +0430

    Second Commit

---
+++ CleanArchitectureT/Emplooye.API/Startup.cs
@@ -0,0 +1,77 @@
+using Employee.Application.Handlers;
+using Employee.Infrastructure.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Reflection;
+using Employee.Core.Repositories.Base;
+using Employee.Infrastructure.Repositories.Base;
+using Employee.Infrastructure.Repositories;
+using Employee.Core.Repositories;
+
+namespace Emplooye.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            
+            services.AddDbContext<EmployeeContext>(m => m.UseSqlServer(Configuration.GetConnectionString("EmployeeDB")), ServiceLifetime.Singleton);
+            
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Employee.API",
+                    Version = "v1"
+                });
+            });
+
+            services.AddAutoMapper(typeof(Startup));
+            services.AddMediatR(typeof(CreateEmployeeHandler).GetTypeInfo().Assembly);
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddTransient<IEmployeeRepository, EmployeeRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ CleanArchitectureT/Employee.Application/Handlers/CreateEmployeeHandler.cs
@@ -0,0 +1,32 @@
+﻿using Employee.Application.Commands;
+using Employee.Application.Mappers;
+using Employee.Application.Responses;
+using Employee.Core.Repositories;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Employee.Application.Handlers
+{
+    public class CreateEmployeeHandler : IRequestHandler<CreateEmployeeCommand, EmployeeResponse>
+    {
+        private readonly IEmployeeRepository _employeeRepo;
+        public CreateEmployeeHandler(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepo = employeeRepository;
+        }
+        public async Task<EmployeeResponse> Handle(CreateEmployeeCommand request, CancellationToken cancellationToken)
+        {
+            var employeeEntity = EmployeeMapper.Mapper.Map<Employee.Core.Entities.Employee>(request);
+            if (employeeEntity is null)
+            {
+                throw new ApplicationException("Issue With mapper");
+            }
+            var newEmployee = await _employeeRepo.AddAsync(employeeEntity);
+            var employeeResponse = EmployeeMapper.Mapper.Map<EmployeeResponse>(newEmployee);
+
+            return employeeResponse;
+        }
+    }
+}
